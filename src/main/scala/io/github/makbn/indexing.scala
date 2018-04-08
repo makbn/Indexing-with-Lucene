2c8a2c2ea396fcd7aea857c8e74205748b1edf65
makbn <mehdi74akbarian@gmail.com>
2018-04-08 21:40:18 +0430

    cleanup

--- src/main/scala/io/github/makbn/indexing.scala
+++ src/main/scala/io/github/makbn/indexing.scala
@@ -8,14 +8,14 @@ import org.apache.lucene.util.Version
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable._
 
-object Indexing {
 
-  private val APP_NAME = "Indexing with Lucene"
-  private val MASTER = "local[*]"
-  private val DATA_FILE_DIR = "src/main/resources/medical.all"
+object indexing {
+
+  private val AppName = "Indexing with Lucene"
+  private val Master = "local[*]"
+  private val DataFileDir = "src/main/resources/medical.all"
   private val analyzer = new StopAnalyzer(Version.LUCENE_40)
  // private val analyzer = new StandardAnalyzer(Version.LUCENE_40)
   private val config = new IndexWriterConfig(Version.LUCENE_40, analyzer)
@@ -41,7 +41,7 @@ object Indexing {
     val doc = new Document()
     doc.add(new Field("text", arg._2, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS))
     doc.add(new Field("id", String.valueOf(arg._1), Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.WITH_POSITIONS_OFFSETS))
-    return doc
+    doc
   }
 
   /**
@@ -53,34 +53,35 @@ object Indexing {
     val regex = ".I\\s\\d{1,}"
     val prescriptions = new ArrayBuffer[(Int, String)]()
     val arr = dataFile.collect()
-    var i = 0
-    var count = 1
+
+    var count : Int = 1
+    var i : Int = 0
     while (i < arr.length) {
-      var prescription = ""
+      var prescription = new StringBuilder
       if (arr(i).matches(regex)) {
         var j = i.+(2)
         while (j < arr.length && !arr(j).matches(regex)) {
-          prescription = prescription.+(arr(j).trim + " ")
+          prescription.appendAll(arr(j).trim + " ")
           j += 1
         }
-        prescriptions += ((count, prescription))
+        prescriptions += ((count, prescription.result))
         i = j.+(-2)
         count += 1
       }
       i += 1
     }
-    return prescriptions.toArray
+    prescriptions.toArray
   }
 
   def indexing() = {
     val sparkConf = new SparkConf()
-      .setAppName(APP_NAME)
-      .setMaster(MASTER)
+      .setAppName(AppName)
+      .setMaster(Master)
     val sc = new SparkContext(sparkConf)
     implicit val sparkContext = sc
 
     val indexWriter = new IndexWriter(ramDirectory, config)
-    val dataFile = sc.textFile(DATA_FILE_DIR)
+    val dataFile = sc.textFile(DataFileDir)
     val prs = pars(dataFile)
 
 
@@ -100,12 +101,12 @@ object Indexing {
       val result = new HashMap[String, HashMap[Int, Long]]()
       val idxReader = DirectoryReader.open(ramDirectory)
       for (i <- 0 to idxReader.numDocs - 1) {
-        val terms: Terms = idxReader.getTermVector(i, "text")
-        if (terms != null) {
-          val termsEnum = terms.iterator(null)
-          var bytesRef = termsEnum.next
-          while (bytesRef != null) {
-            val term = bytesRef.utf8ToString.toLowerCase
+        val terms: Option[Terms] = Option(idxReader.getTermVector(i, "text"))
+        if (terms.isDefined) {
+          val termsEnum = terms.get.iterator(TermsEnum.EMPTY)
+          var bytesRef = Option(termsEnum.next)
+          while (bytesRef.isDefined) {
+            val term = bytesRef.get.utf8ToString.toLowerCase
             if (result.contains(term)) {
               var record = result(term)
               if (record.contains(i))
@@ -118,11 +119,11 @@ object Indexing {
                   put(i, termsEnum.totalTermFreq())
                 }
               })
-            bytesRef = termsEnum.next
+            bytesRef = Option(termsEnum.next)
           }
         }
       }
-      return result
+      result
     }
   }
 }
